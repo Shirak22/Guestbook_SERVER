06089d60d111d171e1fa45e619fd267e40972778
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2023-09-17 19:23:30 +0200

    add cors

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const {connectDB} = require('./configs/db');
 connectDB();
 const cookieParser = require('cookie-parser');
-
 const mainRoute = require('./router/main');
 const authRoute = require('./router/auth');
+const cors = require('cors');
 
 
 
@@ -15,14 +15,16 @@ const authRoute = require('./router/auth');
 app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(cors());
 app.use('/api/',mainRoute); 
 app.use('/api/auth',authRoute); 
+
 app.all('*',(req,res)=> {
     res.status(404).json({success:false,message: '404 The page not found! '}); 
 })
 
 
-app.listen(3000,()=> console.log('The server is running on port 3000')); 
+app.listen(process.env.PORT || 4000,()=> console.log('The server is running on port 3000')); 
 
 
 
