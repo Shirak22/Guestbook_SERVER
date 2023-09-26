1391debab4770e88a42b57ef825b412510c48020
Shirak <shirak_serop_6@hotmail.com>
2023-09-26 15:29:55 +0100

    adding database connection variables in env file

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose'); 
 const db_uri = process.env.DB_CONNECT; 
 
+const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+  } = process.env;
+
+  
 const connectDB = async () => {
+    const uri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authMechanism=DEFAULT&authSource=admin`; 
     try {
-        const conn = await mongoose.connect(db_uri);
+        const conn = await mongoose.connect(uri);
         console.log('connected on host: ' + conn.connection.host); 
     } catch (error) {
         console.log(error);
